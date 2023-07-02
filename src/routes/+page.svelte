14e3954c472ccb904daca3619a88dc6c49a29d1b
Zek <zek@pop-os.localdomain>
2023-07-01 21:10:27 -0400

    Text borders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,32 @@
-
 <div>
   <h1>Ezequiel Valencia</h1>
   <dl>
-    <dt>
+    <dt id="about_me_dt">
       <a href="/about_me">About Me</a>
     </dt>
-    <dt>
+    <dt id="school_dt">
       <a href="/school">Education Projects</a>
     </dt>
-    <dt>
+    <dt id="personal_dt">
       <a href="/personal_projects">Personal Projects</a>
     </dt>
-    <dt>
+    <dt id="company_dt">
       <a href="/company">Company Projects</a>
     </dt>
   </dl>
 </div>
 
 <style lang="scss">
-
-  @mixin textBorder($borderIcon: DarkGrey) {
-    background: $borderIcon;
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Backgrounds_and_Borders/Border-image_generator
+  @mixin textBorder($borderIcon: DarkGrey, $slice: 80, $repeat: repeat, $textColor: aqua) {
+    border: 15px solid;
+    border-width: 1vw;
+    border-image-source: $borderIcon;
+    border-image-repeat: $repeat;
+    border-image-slice: $slice;
+    a:hover {
+      color: $textColor;
+    }
   }
 
   div {
@@ -36,9 +42,22 @@
 
   a {
     color:orangered;
-    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
-      sans-serif;
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
   }
+
+  #about_me_dt:hover {
+    @include textBorder($borderIcon: url('/Text-borders/wet-leaf.png'));
+  }
+
+  #company_dt:hover{
+    @include textBorder(linear-gradient(90deg, red, blue), $slice: 1, $repeat: auto)
+  }
+
+  #school_dt:hover{
+        // fds
+  }
+
+
 
   h1 {
     text-align: center;

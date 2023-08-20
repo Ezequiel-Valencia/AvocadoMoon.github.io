6658978b589fb426ecb6a9375162f9075fd69c5f
Ezequiel Valencia <casualcucumber59@gmail.com>
2023-08-20 08:40:17 -0400

    Add some color and main div

--- src/routes/about_me/+page.svelte
+++ src/routes/about_me/+page.svelte
@@ -1,1 +1,32 @@
-<h1> About me</h1>
+<script lang="ts">
+</script>
+
+<div id="main-div">
+
+    <div id="intro">
+        <img alt="Ezequel">
+
+    </div>
+
+  <h1>About me</h1>
+
+</div>
+
+<style lang="scss">
+
+  #main-div {
+    height: 100%;
+    width: 100%;
+    top: 0px;
+    left: 0px;
+    padding: 0%;
+    position: absolute;
+  }
+
+  #intro {
+    height: 100vh;
+    width: 100vw;
+    background-color:rgb(4, 70, 4);
+  }
+
+</style>

082a9dcaa004a7ac7af769c51da2889ff5eaee5e
Ezequiel Valencia <casualcucumber59@gmail.com>
2023-08-28 14:58:37 -0400

    Basic Zine page

--- src/routes/zines/+page.svelte
+++ src/routes/zines/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
+    import Intro from "./intro.svelte";
+  import Zines from "./zines.svelte";
 </script>
 
 
 
 <div id="main-div">
-    <div id="intro">
-        <div id="gold-border">
-            <h1 id="zine-intro">Zines</h1>
-        </div>
-    </div>
+    <Intro></Intro>
+    <Zines></Zines>
 </div>
 
 
@@ -21,35 +20,4 @@
         height: 100%;
         width: 100%;
     }
-
-    #intro{
-        height: 100vh;
-        width: 100vw;
-        background-color: rgb(7, 40, 230);
-    }
-
-    #gold-border{
-        height: 80vh;
-        width: 80vw;
-        border-style: groove;
-        
-        top: 50%;
-        transform: translateY(-50%);
-        margin: auto;
-        position: relative;
-    }
-
-    #zine-intro{
-        position: relative;
-        margin: 0%;
-        padding: 0%;
-        
-        top: 50%;
-        transform: translateY(-50%);
-        text-align: center;
-        color: rgb(161, 140, 19);
-    }
-    
-
-
 </style>

32d867ab5f873426617a21941d3e5fd013e23180
Ezequiel Valencia <casualcucumber59@gmail.com>
2023-08-27 21:13:49 -0400

    Center text and border

--- src/routes/zines/+page.svelte
+++ src/routes/zines/+page.svelte
@@ -1,1 +1,55 @@
-<h1>Zines</h1>
+<script lang="ts">
+</script>
+
+
+
+<div id="main-div">
+    <div id="intro">
+        <div id="gold-border">
+            <h1 id="zine-intro">Zines</h1>
+        </div>
+    </div>
+</div>
+
+
+
+<style lang="scss">
+    #main-div{
+        position: absolute;
+        left: 0%;
+        top: 0%;
+        height: 100%;
+        width: 100%;
+    }
+
+    #intro{
+        height: 100vh;
+        width: 100vw;
+        background-color: rgb(7, 40, 230);
+    }
+
+    #gold-border{
+        height: 80vh;
+        width: 80vw;
+        border-style: groove;
+        
+        top: 50%;
+        transform: translateY(-50%);
+        margin: auto;
+        position: relative;
+    }
+
+    #zine-intro{
+        position: relative;
+        margin: 0%;
+        padding: 0%;
+        
+        top: 50%;
+        transform: translateY(-50%);
+        text-align: center;
+        color: rgb(161, 140, 19);
+    }
+    
+
+
+</style>

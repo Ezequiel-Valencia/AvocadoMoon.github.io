bd1a2576b3f4c3491d91baf4733038591f7ee532
Zek <zek@pop-os.localdomain>
2023-09-10 08:52:54 -0400

    Added some more scalfolding to education channel

--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -1,4 +1,9 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import Menu from "../menu.svelte";
+  import Edu from "./edu.svelte";
+  import Intro from "./intro.svelte";
+
+</script>
 
 
 
@@ -6,7 +11,9 @@
 
 
 <div id="main-div">
-
+    <Menu email=""></Menu>
+    <Intro></Intro>
+    <Edu></Edu>
 </div>
 
 
@@ -14,5 +21,10 @@
 
 
 <style lang="scss">
+    #main-div{
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
     
 </style>
--- src/routes/education/intro.svelte
+++ src/routes/education/intro.svelte
@@ -1,15 +1,38 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { onMount } from "svelte";
 
+  //
+  var isVisible = true;
+  onMount(() => {
+    function isElementInViewport(el: any) {
+      const rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <=
+          (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
 
+    function handleScroll() {
+      const element = document.querySelector(".animate-me");
+      if (isElementInViewport(element)) {
+        element?.classList.add("animate");
+        window.removeEventListener("scroll", handleScroll);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+  });
+</script>
 
 <div>
-
+    {#if isVisible}
+    <div class="animate-me"> </div>
+    {/if}
 </div>
 
-
-
-
-
 <style lang="scss">
-    
 </style>

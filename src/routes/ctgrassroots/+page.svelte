a18e7b3b6aebbb3756b024552d8fd1ff6228b14e
Zek <zek@pop-os.localdomain>
2023-11-19 15:34:10 -0500

    Text in FAQ

--- src/routes/ctgrassroots/+page.svelte
+++ src/routes/ctgrassroots/+page.svelte
@@ -1,38 +1,30 @@
 <script lang="ts">
-  import { AccordionItem, Accordion } from 'flowbite-svelte';
+  import Menu from "../menu.svelte";
+  import Faq from "./FAQ.svelte";
+  import { musicController } from "../../myLocalStorage";
 </script>
 
+<div id="main-div">
+  <Menu email="website@worker.slmail.me" songName="Zora's Domain - Day (Breath of the Wild)"></Menu>
+  <Faq></Faq>
 
-<div>
-  <Accordion>
-    <AccordionItem>
-      <span slot="header">My Header 1</span>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-      <p class="text-gray-500 dark:text-gray-400">
-        Check out this guide to learn how to <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> get started </a>
-        and start developing websites even faster with components on top of Tailwind CSS.
-      </p>
-    </AccordionItem>
-    <AccordionItem>
-      <span slot="header">My Header 2</span>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ab necessitatibus sint explicabo ...</p>
-      <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-      <ul class="list-disc pl-5 dark:text-gray-400 text-gray-500">
-        <li>
-          <a href="/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline"> Lorem ipsum </a>
-        </li>
-        <li>
-          <a href="https://tailwindui.com/" rel="noreferrer" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline"> Tailwind UI </a>
-        </li>
-      </ul>
-    </AccordionItem>
-  </Accordion>
 
+  {#if $musicController}
+      <audio id="song" src="/Audio/Channel-Songs/Zora's Domain - Day (Breath of the Wild).mp3" autoplay loop></audio>
+  {/if}
+  
 </div>
 
 
-
 <style lang="scss">
+  #main-div{
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      background-color: #145e0e;
+      overflow: auto;
+  }
 
 </style>
---
+++ src/routes/ctgrassroots/explination.ts
@@ -0,0 +1,105 @@
+
+export const exp = [
+    {
+        header: "Easier to find activities",
+        explanation: `The CTGrassRoots website is an instance of mobilizon, 
+        a web application who's purpose is to facilitate in person gatherings. Some of it's
+        features are:
+        <ul>
+            <li>Find events within a radius of a given location.</li>
+            <li>Filter events based on category.</li>
+            <li>Create groups to post events, list community resources, and have discussions.</li>
+        </ul>`
+    },
+    {
+        header: "What does it mean to be a mobilizon instance?",
+        explanation: `Mobilizon is a web application that can be copied and recreated
+        by anyone who has the source code, and the developers actually emphasis trying to 
+        make your own instances since it helps to disperse the power of just a singular instance.
+        These separate instances can still communicate with each other though, creating a form
+        of connected web allowing for all of these different instances to still feel as if it's
+        one large website.`
+    },
+    {
+        header: "If Mobilizon's code is already built by other people then what did CTGrassRoots have to do?",
+        explanation: `Yes, I did not write Mobilizon software, but writing the web application software
+        is only part of getting a website working. I like to think of it as building a car, if the web application
+        software is already built then that's the engine being done, but there's still all the other components that
+        a car needs to function. For example:
+        <ul>
+            <li>The body of a car which holds the people (database)</li>
+            <li>Pedals and steering wheel (server interface, mailserver, domains, etc...)</li>
+            <li>Safety features to prevent crashing and help diagnosis mechanical failures (data backups, logging, server security principles, etc...)</li>
+            <li>Luxury features to make the ride more smooth (HPC clusters, database caching, high-availability, api integration, etc...)
+        </ul>
+        So even though one key component is already made plenty of other components need to be outsourced
+        , built together making all pieces fit, then when done constantly maintained to ensure the car
+        never stops functioning as a car.`
+    },
+    {
+        header: "FOSS and Donations",
+        explanation: `Although Mobilizon and the work I am doing to host this website follows the 
+        FOSS (Free Open Source Software) mentality it does not mean that the labor put into
+        creating this is free; for the free in FOSS represents unconstricted software that can be placed
+        anywhere, not monetarily free.
+        
+        Thus, if you want to help off put the server costs for hosting this site, allow me to upgrade
+        the site itself, and compensate me for all the time I've put into this, please donate to my 
+        patreon <a href="">here</a>.`
+    },
+    {
+        header: "Privacy Policy",
+        explanation: `I'm not doing anything with the data provided except having it sit on my server.
+        I never plan on selling anyone's data or implementing some sort of ad's system to make money.
+        This means though that this server has a hard limit as to how much I'm willing to spend hosting it.
+        If you want to help make this spending limit larger to have a better service, please look at the
+        FOSS and Donations tab.`
+    },
+    {
+        header: "Creating Community and CTGrassRoots Goal",
+        explanation: `The main goal of CTGrassRoots is to help facilitate the growth of local organzine where ever 
+        you may live in the CT area. A lot of time is spent on social media applications, but little to no time being
+        social. Their algorithms keep you within a digital community that may not be representative
+        of your physical community, and dampens introduction of new ideas/growth. 
+        When in the normal school years of K-12-College there are extracurricular activities to promote
+        social engagement, and immediately afterwards that social net is gone. Clubs that have people gather around
+        an interest still exist but are difficult to find. Some ways to find events now are:
+        <ul>
+            <li> Be lucky and find a physical flyer in the streets. </li>
+            <li> Happen to have a friend which know's about a spot. </li>
+            <li> Social media algorithm throws a digital flyer your way in the sea of content it has. </li>
+        </ul>
+        Each method has its own merit, but it requires luck and a clunky method for keeping track of 
+        events local to yourself. This website is meant to take that aspect of luck away, and allow
+        you to focus only on growing your community, finding new ones, or just something to do
+        on your time off.`
+    },
+    {
+        header: "Does this replace social media apps?",
+        explanation: `No, not at all. Social media apps are meant for sharing moments with people
+        in the digital realm, centered around entertaining the user to stay on the application.
+        This websites goal is for in person gatherings, and will be for nothing more since it 
+        doesn't need to be a super app.`
+    },
+    {
+        header: "How can I the user help?",
+        explanation: `There is plenty of ways to help CTGrassRoots them being:
+        <ul>
+            <li> Background in IT/Cybersecurity, audit the server or code base used to have the server running </li>
+            <li> Grow your community wherever you live so the website can have a wider selection of events,
+            thus having a larger group of acceptance. </li>
+            <li> Report any malicious behavior and don't be an asshole. </li>
+            <li> Make your own instance of Mobilizon and connect our instances together </li>
+            <li> Promote the website and other FOSS softwares </li>
+            <li> Donate if you have the income to do so </li>
+        </ul>
+        If you want to learn more about computer science, it's effect on communities stability, and the
+        significance it has within our daily lives please check out my Zine "Pandora's Deus Ex Machina".`
+    },
+    {
+        header: "Future Work",
+        explanation: `Although having my site up is great there's still plenty of work to be done,
+        both in making my site better, helping other people to have their own websites, and in 
+        general foster a better community.`
+    },
+]

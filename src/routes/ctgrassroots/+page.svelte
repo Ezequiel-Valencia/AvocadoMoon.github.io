2df1b14510213bf492c643c528b812d6a137f1cb
Zek <zek@pop-os.localdomain>
2023-11-19 16:16:33 -0500

    Add Title

--- src/routes/ctgrassroots/+page.svelte
+++ src/routes/ctgrassroots/+page.svelte
@@ -5,26 +5,50 @@
 </script>
 
 <div id="main-div">
+
   <Menu email="website@worker.slmail.me" songName="Zora's Domain - Day (Breath of the Wild)"></Menu>
+
+  <div id="intro">
+    <h1 id="ctheader">
+      CTGrassRoots
+    </h1>
+  </div>
+
   <Faq></Faq>
 
 
-  {#if $musicController}
-      <audio id="song" src="/Audio/Channel-Songs/Zora's Domain - Day (Breath of the Wild).mp3" autoplay loop></audio>
-  {/if}
+  <!-- {#if $musicController}
+  {/if} -->
   
 </div>
 
 
 <style lang="scss">
   #main-div{
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 100%;
-      width: 100%;
-      background-color: #145e0e;
-      overflow: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: #145e0e;
+    overflow: auto;
+  }
+  #intro{
+    height: 80vh;
+    width: 100vw;
+    // background-color: rgb(15, 34, 139);
+  }
+
+  #ctheader{
+    font-family: 'Playfair Display', serif;
+        
+    text-align: center;
+    color: goldenrod;
+    top: 50%;
+    transform: translateY(-50%);
+    position: relative;
+    margin: 0%;
+    padding: 0%;
   }
 
 </style>

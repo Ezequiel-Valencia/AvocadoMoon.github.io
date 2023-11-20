48f0b24c7eaa5dc17d3ad6824727b07aa78c8fd9
Zek <zek@pop-os.localdomain>
2023-11-20 17:34:28 -0500

    FAQ

--- src/routes/ctgrassroots/FAQ.svelte
+++ src/routes/ctgrassroots/FAQ.svelte
@@ -8,12 +8,16 @@
 
 
 <article id="accordion">
-    <Accordion>
+    <Accordion 
+    inactiveClass="text-gray-700 dark:text-gray-400 hover:bg-gray-100 hover:dark:bg-gray-800"
+    activeClass="bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800">
       {#each exp as panel}
       <div class="accord-item">
-        <AccordionItem>
+        <AccordionItem borderClass="border-l border-r group-first:border-t" 
+        activeClass="border-gray-300"
+        defaultClass="flex items-center justify-between w-full font-medium text-left group-first:rounded-t-xl border-gray-200 dark:border-gray-700">
           <span slot="header">{panel.header}</span>
-          <p class="explination">
+          <p class="explanation">
             {@html panel.explanation}
           </p>
         </AccordionItem>
@@ -31,5 +35,9 @@
     margin-right: auto;
     width: 80vw;
     max-width: 1000px;
+  }
+
+  .explanation {
+    color: black;
   }
 </style>

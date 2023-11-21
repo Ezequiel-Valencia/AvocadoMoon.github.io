8d367dd6eda5028b73d1781658aa5c104ae93e81
Zek <zek@pop-os.localdomain>
2023-11-20 19:54:19 -0500

    Fix grammar and add patreon

--- src/routes/ctgrassroots/explination.ts
+++ src/routes/ctgrassroots/explination.ts
@@ -9,21 +9,22 @@ export const exp = [
             <li>Find events within a radius of a given location.</li>
             <li>Filter events based on category.</li>
             <li>Create groups to post events, list community resources, and have discussions.</li>
-        </ul>`
+        </ul>
+        Click <a href="https://ctgrassroots.org/">here</a> to access CTGrassRoots and use this website.`
     },
     {
         header: "What does it mean to be a mobilizon instance?",
         explanation: `Mobilizon is a web application that can be copied and recreated
-        by anyone who has the source code, and the developers actually emphasis trying to 
-        make your own instances since it helps to disperse the power of just a singular instance.
-        These separate instances can still communicate with each other though, creating a form
-        of connected web allowing for all of these different instances to still feel as if it's
+        by anyone who has the source code. The developers actually emphasis trying to 
+        make your own instances for it helps to disperse the power of just a singular instance.
+        These separate instances can still communicate with each other, creating a
+        connected web allowing all these different instances to feel as if it's
         one large website.`
     },
     {
-        header: "If Mobilizon's code is already built by other people then what did CTGrassRoots have to do?",
-        explanation: `Yes, I did not write Mobilizon software, but writing the web application software
-        is only part of getting a website working. I like to think of it as building a car, if the web application
+        header: "If Mobilizon's code is already built by other people then what work went into building CTGrassRoots?",
+        explanation: `Although the web application code is a key component to having a functional websit
+        it's not the only one. I like to think of it as building a car, if the web application
         software is already built then that's the engine being done, but there's still all the other components that
         a car needs to function. For example:
         <ul>
@@ -33,7 +34,7 @@ export const exp = [
             <li>Luxury features to make the ride more smooth (HPC clusters, database caching, high-availability, api integration, etc...)
         </ul>
         So even though one key component is already made plenty of other components need to be outsourced
-        , built together making all pieces fit, then when done constantly maintained to ensure the car
+        , built together, then when done constantly maintained to ensure the car
         never stops functioning as a car.`
     },
     {
@@ -45,7 +46,7 @@ export const exp = [
         
         Thus, if you want to help off put the server costs for hosting this site, allow me to upgrade
         the site itself, and compensate me for all the time I've put into this, please donate to my 
-        patreon <a href="">here</a>.`
+        patreon <a href="https://www.patreon.com/SystemSimulacrum">here</a>.`
     },
     {
         header: "Privacy Policy",
@@ -57,7 +58,7 @@ export const exp = [
     },
     {
         header: "Creating Community and CTGrassRoots Goal",
-        explanation: `The main goal of CTGrassRoots is to help facilitate the growth of local organzine where ever 
+        explanation: `The main goal of CTGrassRoots is to help facilitate the growth of local organization where ever 
         you may live in the CT area. A lot of time is spent on social media applications, but little to no time being
         social. Their algorithms keep you within a digital community that may not be representative
         of your physical community, and dampens introduction of new ideas/growth. 
@@ -77,7 +78,7 @@ export const exp = [
     {
         header: "Does this replace social media apps?",
         explanation: `No, not at all. Social media apps are meant for sharing moments with people
-        in the digital realm, centered around entertaining the user to stay on the application.
+        in the digital realm centered around entertaining the user to stay on the application.
         This websites goal is for in person gatherings, and will be for nothing more since it 
         doesn't need to be a super app.`
     },
@@ -88,12 +89,12 @@ export const exp = [
             <li> Background in IT/Cybersecurity, audit the server or code base used to have the server running </li>
             <li> Grow your community wherever you live so the website can have a wider selection of events,
             thus having a larger group of acceptance. </li>
-            <li> Report any malicious behavior and don't be an asshole. </li>
-            <li> Make your own instance of Mobilizon and connect our instances together </li>
-            <li> Promote the website and other FOSS softwares </li>
-            <li> Donate if you have the income to do so </li>
+            <li> Report any malicious behavior and be kind. </li>
+            <li> Make your own instance of Mobilizon and connect our instances together. </li>
+            <li> Promote the website and other FOSS softwares. </li>
+            <li> Donate if you have the income to do so. </li>
         </ul>
-        If you want to learn more about computer science, it's effect on communities stability, and the
+        If you want to learn more about computer science, it's effect on community stability, and the
         significance it has within our daily lives please check out my Zine "Pandora's Deus Ex Machina".`
     },
     {

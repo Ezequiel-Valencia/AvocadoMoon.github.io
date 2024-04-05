229a3e8250c377f3add120aad052b90330213df8
Zek <zek@pop-os.localdomain>
2024-04-05 19:59:09 -0400

    Remove Unecessary classes from flowbite

--- src/routes/about_me/hobbies.svelte
+++ src/routes/about_me/hobbies.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { Tabs } from 'flowbite';
-  import type { TabsOptions, TabsInterface, TabItem } from 'flowbite';
-  import { onMount } from 'svelte';
   import { hobbies } from './activites';
 
   // https://flowbite.com/docs/components/tabs/#typescript
@@ -9,40 +6,9 @@
   const tabCSSClass = "inline-block p-5 border-b-2 rounded-t-lg"
   const tabContainerCSSClass = "flex flex-wrap -mb-px text-sm font-medium text-center"
   const listClass = "flex flex-wrap -mb-px text-sm font-medium text-center";
-  const dynamicContent = "hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-
+  
   $: activeIndex = 0;
   
-  function showDivContent(){
-    
-  }
-
-  onMount(() => {
-    const tabContainer: HTMLElement = document.getElementById("tabs") as HTMLElement;
-    const tabElements: TabItem[] = []
-    const options: TabsOptions = {
-      defaultTabId: 'Athletic',
-      activeClasses:
-        'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
-      inactiveClasses:
-        'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
-      onShow: () => {
-        console.log('tab is shown');
-    }
-    }
-
-    var tabs = document.getElementsByClassName("tab") as HTMLCollectionOf<HTMLElement>;
-    for (var element of tabs){
-      tabElements.push({
-        id: element.id,
-        triggerEl: element,
-        targetEl: element
-      })
-    }
-    
-    // Initialize the tabs
-    
-  });
 </script>
 
 <div id="hobbies">

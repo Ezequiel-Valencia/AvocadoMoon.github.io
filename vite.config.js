48da0e70f3bc462ad9d3a3022d77cca017ce1c27
Zek <zek@pop-os.localdomain>
2024-04-08 20:09:38 -0400

    Window context rollup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  build: {
+    rollupOptions: {
+      context: 'window'
+    }
+  },
 
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],

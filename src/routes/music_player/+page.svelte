d52b4130a595894b7683830fe4e3ac76a9f1ce48
Zek <zek@pop-os.localdomain>
2024-06-26 12:48:34 -0400

    Music Channel Scalfolding

---
+++ src/routes/music_player/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Menu from "../menu.svelte";
+
+</script>
+
+
+
+<div id="main-div">
+    <Menu email="website@worker.slmail.me" songName="Zora's Domain - Day (Breath of the Wild)"></Menu>
+</div>
+
+
+<style lang="scss">
+
+</style>

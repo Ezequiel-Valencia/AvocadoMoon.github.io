5b6f246f100a2d8507cc5da87da61e511dce9918
Zek <zek@pop-os.localdomain>
2024-07-29 17:24:14 -0400

    Init Cat Channel

---
+++ src/routes/cat_encryption/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Menu from "../menu.svelte";
+
+</script>
+
+
+
+<div id="main-div">
+    <Menu email="website@worker.slmail.me" songName="Meow - Ivusm"></Menu>
+
+    
+</div>
+
+
+<style lang="scss">
+
+</style>

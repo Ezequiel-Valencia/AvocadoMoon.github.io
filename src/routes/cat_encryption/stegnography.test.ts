d9c2a544f85f340aa113e7cbf8fcb50949623c4c
Zek <zek@pop-os.localdomain>
2024-08-03 19:48:03 -0400

    Random Tests For Stegnography

--- src/routes/cat_encryption/stegnography.test.ts
+++ src/routes/cat_encryption/stegnography.test.ts
@@ -2,6 +2,20 @@ import { describe, expect, test, vi } from "vitest";
 import { encodeImage, decodeImage } from "./stegnography";
 import { decryptMessage, encryptMessage } from "./encryption";
 
+
+function getRandomInt(max: number){
+    return Math.floor(Math.random() * max)
+}
+
+function getRandomString(stringLen: number){
+    let resultString = ""
+
+    for(let i = 0; i < stringLen; i++){
+        resultString += String.fromCharCode(getRandomInt(255))
+    }
+    return resultString;
+}
+
 test("Encode and Decode Message", () => {
     let width = 10
     let height = 10
@@ -14,6 +28,39 @@ test("Encode and Decode Message", () => {
 
     let encodedImage = encodeImage(message, originalArrayImage, width, height);
     let decodedMessage = decodeImage(encodedImage, width, height)
+
+    expect(decodedMessage).toBe(message)
+
+    width = getRandomInt(100)
+    height = getRandomInt(100)
+
+    message = getRandomString(getRandomInt((width * height) / 4))
+    originalArrayImage = new Array(width * height * 4).fill(getRandomInt(255))
+    encodedImage = encodeImage(message, originalArrayImage, width, height)
+    decodedMessage = decodeImage(encodedImage, width, height)
+
+    expect(decodedMessage).toBe(message)
+})
+
+test("Max Message Length", () => {
+    let width = 2
+    let height = 2
+
+    let message = "Max" //one less than message length because of header
+
+
+    // process.stdout.write("Going to encode \n")
+    let originalArrayImage = new Array(width * height * 4).fill(255)
+
+    let encodedImage = encodeImage(message, originalArrayImage, width, height);
+    let decodedMessage = decodeImage(encodedImage, width, height)
+
+    expect(decodedMessage).toBe(message)
+
+    let beyondMax = message + "jj"
+
+    encodedImage = encodeImage(beyondMax, originalArrayImage, width, height)
+    decodedMessage = decodeImage(encodedImage, width, height)
 
     expect(decodedMessage).toBe(message)
 })
@@ -38,7 +85,6 @@ test("Encrypt-Encode and Decrypt-Decode", async () =>{
 
     let originalArrayImage = new Array(width * height * 4).fill(0)
     let encryptedMessage = await encryptMessage(unencryptedMessage)
-    process.stdout.write("Decrypt")
 
     // let stringEncryptedMessage = new TextDecoder().decode(new Uint8Array(encryptedMessage.encrypted))
     let encodedImage = encodeImage(encryptedMessage.encrypted, originalArrayImage, width, height);

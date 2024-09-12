19142391b4a6dc75cd9c0e9d865d5e3a54f864f3
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-09-11 20:18:07 -0400

    Make Link In Stegnography Page Visible

--- src/routes/cat_encryption/+page.svelte
+++ src/routes/cat_encryption/+page.svelte
@@ -24,7 +24,7 @@
 
     <div id="title" style="">
         <h1 id="title-text" class="text">Cat Communication</h1>
-        <p class="text">Hiding your message in cat images using <a href="https://en.wikipedia.org/wiki/Steganography">Stenography</a>.</p>
+        <p class="text">Hiding your message in cat images using <a style="color:cadetblue;" href="https://en.wikipedia.org/wiki/Steganography">Stenography</a>.</p>
     </div>
 
     <canvas id="matrixCanvas"></canvas>
--- tests/test.js
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-import { encodeImage, decodeImage } from '../src/routes/cat_encryption/stegnography';
-
-// test('index page has expected h1', async ({ page }) => {
-// 	await page.goto('/');
-// 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
-// });

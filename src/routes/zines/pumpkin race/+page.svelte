7d0969920d7e973155e5d120942a5b7e020e90c3
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-09-20 14:26:54 -0400

    How to Race A Pumpkin

---
+++ src/routes/zines/pumpkin race/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { zines, type zineMetaData } from "../zine";
+  import DisplayZine from "../displayZine.svelte";
+  import Menu from "../../../common/menu.svelte";
+  let zine: zineMetaData = zines.pumpkinRace
+
+</script>
+
+
+
+
+
+<div style="position: absolute; top: 0; left: 0; text-align:center;
+ height:100%; width: 100%;
+ ">
+    <Menu email="" songName=""></Menu>
+    <DisplayZine zine={zines.pumpkinRace}></DisplayZine>
+</div>
+
+
--- src/routes/zines/zine.ts
+++ src/routes/zines/zine.ts
@@ -14,15 +14,26 @@ export interface zineMetaData {
 const thirdSpaceDir = dirPrefix + "/Third Space"
 const pandoraDir = dirPrefix + "/pandora"
 const emotionlessDir = dirPrefix + "/emotionless"
+const pumpkinRaceDir = dirPrefix + "/pumpkin race"
 
 export const zines: {[key: string]: zineMetaData} = {
+    pumpkinRace:
+    {
+        zineCover: pumpkinRaceDir + "/Fall Zine.png",
+        title: "How to Race A Pumpkin",
+        name: "pumpkin race",
+        description: `Tips on how to race a pumpkin based on first hand experience.`,
+        redirect: pumpkinRaceDir,
+        backgroundImage: pumpkinRaceDir + "/back.jpg",
+        pdfPath: pumpkinRaceDir + "/Fall Zine.pdf"
+    },
     thirdSpace:
     {
         zineCover: thirdSpaceDir + "/Third Space.webp",
         title: "Encounters of the Third Space",
         name: "Third Space",
-        description: `My most recent and ambitious Zine that discusses the current loneliness epidemic 
-        within America, land use, and predatory technology.`,
+        description: `Analysis of the current loneliness epidemic 
+        within America, land use, and predatory technology enriched by introspection.`,
         redirect: thirdSpaceDir,
         backgroundImage: "/zines/pandora/back.jpg",
         pdfPath: thirdSpaceDir + "/Third Space.pdf"

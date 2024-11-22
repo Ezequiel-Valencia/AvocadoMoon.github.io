07eac77bf92c272d82dc81a1374ace9e245b6ef0
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-11-22 15:28:54 -0500

    Continous Gradient in About Me

--- src/routes/about_me/+page.svelte
+++ src/routes/about_me/+page.svelte
@@ -5,22 +5,37 @@
 </script>
 
 <span id="main-div">
+  <span id="background-gradient">
+
+  </span>
   <Menu songPath="https://files.catbox.moe/ll8iaa.mp3" email="general@relaxed.slmail.me" songName="Samurai Champloo Aruarian Dance"/>
   <Me />
   <Hobbies />
 
 </span>
 
+
+
 <!-- https://www.youtube.com/watch?v=vJNVramny9k -->
 <!-- Squircle is a lot easier so that for now -->
 
 <style lang="scss">
   #main-div {
-    // overflow:visible;
     position: absolute;
     left: 0%;
     top: 0%;
+    
+  }
+
+  #background-gradient{
+    position: absolute;
+    z-index: -2;
     height: 100%;
     width: 100%;
+    background-size: 300%;
+    --gradient: linear-gradient(
+       #339996, #329962, #12693b
+    );
+    background-image: var(--gradient);
   }
 </style>

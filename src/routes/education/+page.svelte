b7ab358b1e33b749a4d5828f2028f7daa2b21410
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-11-23 21:17:20 -0500

    Update Education Channel to Have Light Cursor

--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Menu from "../../common/menu.svelte";
+  import Education from "./education.svelte";
   import Intro from "./intro.svelte";
 
 </script>
@@ -12,6 +13,7 @@
 <div id="main-div">
     <Menu songPath="https://files.catbox.moe/p5bsgp.mp3" email="website@worker.slmail.me" songName="Mozart Symphony 40"></Menu>
     <Intro></Intro>
+    <Education></Education>
 </div>
 
 
@@ -23,7 +25,7 @@
         position: absolute;
         top: 0;
         left: 0;
-        background-color: #d1b698;
+        background-color: #523f29;
         overflow: auto;
         height: auto;
     }

2ad1381135175d20ac1ef2b6c82f4367200fb33a
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-11-23 21:17:20 -0500

    Update Education Channel to Have Light Cursor

---
+++ src/routes/education/education.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+    onMount(() => {
+        const mask: HTMLElement | null = document.querySelector(".black-over-top");
+        document.addEventListener('pointermove', (pos) => {
+            let div = pos.target as HTMLDivElement;
+            let rect = div.getBoundingClientRect()
+            let x = pos.clientX - rect.left;
+            let y = pos.clientY - rect.top;
+            if (0 <= rect.top){
+                mask?.style.setProperty('--mouse-x', x + 'px');
+                mask?.style.setProperty('--mouse-y', y + 'px');
+            } else{
+                mask?.style.setProperty('--mouse-x', 200 + '%');
+                mask?.style.setProperty('--mouse-y', 200 + '%');
+            }
+            
+            
+        })
+    })
+</script>
+
+
+<section id="all-education-content">
+    <div class="black-over-top"></div>
+    <div class="education-content">
+        <article>
+            <div>
+            <h3 class="degree">Bachelors Degree In Cybersecurity</h3>
+            <h2 class="institution">University of Connecticut</h2>
+            <h5>GPA: 3.7</h5>
+            <p style="margin: 4%;">
+                Getting my education in Cybersecurity was one of the best decisions
+                I've made. The subject requires a grasp on all CS subjects for
+                setting a secure enterprise system. That is network, OS, hardware, front-end,
+                back-end, and CI/CD security. With this knowledge I can both excel in personal projects
+                and in the work field.
+            </p>
+            </div>
+            <img class="img" src="/Education/UConn.JPG" alt="Statue">
+        </article>
+    </div>
+</section>
+
+
+
+<style lang="scss">
+    #all-education-content{
+        height: auto;
+        width: auto;
+    }
+    .black-over-top{
+        --mouse-x: 150%;
+        --mouse-y: 150%;
+        background-color: rgb(0, 0, 0);
+        position: absolute;
+        width: 100vw;
+        height: 100%;
+        z-index: 2;
+        mask-image: radial-gradient(
+            circle at var(--mouse-x) var(--mouse-y),
+             rgba(255, 255, 255, 0) 2vmin, rgb(255, 255, 255) 40vmin
+        );
+        cursor: url("/Education/book-cursor.png"), auto;
+    }
+
+    .degree {
+    font-family: "Dancing Script", cursive;
+    font-size: 2em;
+  }
+
+    h2, p, h3, h5{
+        color: white;
+    }
+
+  .education-content {
+    padding-top: 10%;
+    padding-bottom: 10%;
+    width: 90vw;
+    height: auto;
+    text-align: center;
+    margin: auto;
+    position: absolute;
+    z-index: 1;
+  }
+
+  .img {
+    max-height: 45vh;
+    max-width: 35vw;
+    border-style: solid;
+    border-color: white;
+    border-width: 2px;
+  }
+
+  article {
+    height: auto;
+    width: auto;
+    display: flex;
+  }
+
+</style>
--- src/routes/education/intro.svelte
+++ src/routes/education/intro.svelte
@@ -4,45 +4,33 @@
   
 </script>
 
-<div id="intro-div">
-  <div style="height: 100vh;">
+
+<section id="intro-div" style="height: 100vh;">
+  <div style="height: 100vh;background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.99));">
     <div class="content-div" style="top: 25%; margin-bottom:25%">
       <article>
-        <img class="img" src="/Education/dark library.jpg" alt="Library" />
         <div>
-          <h2 class="right-side-text" style="margin-bottom: 2vh;">Education</h2>
           <p class="right-side-text" style="margin-top:0; font-size:2.5em;">
-            "I think wanting to learn stuff is neat." - me
+            “Education is the movement from darkness to light.” – Allan Bloom
           </p>
         </div>
-      </article>
-    </div>
-
-    <div class="content-div">
-      <article>
-        <div>
-          <h3 class="degree">Bachelors Degree In Cybersecurity</h3>
-          <h2 class="institution">University of Connecticut</h2>
-          <h5>GPA: 3.7</h5>
-          <p style="margin: 4%;">
-            Getting my education in Cybersecurity was one of the best decisions
-            I've made. The subject requires a grasp on all CS subjects for
-            setting a secure enterprise system. That is network, OS, hardware, front-end,
-            back-end, and CI/CD security. With this knowledge I can both excel in personal projects
-            and in the work field.
-          </p>
-        </div>
-        <img class="img" src="/Education/UConn.JPG" alt="Statue">
       </article>
     </div>
   </div>
-</div>
+</section>
 
 <style lang="scss">
   @import url("https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap");
   #intro-div {
     height: auto;
     width: 100vw;
+    background-image: url("https://bh.novlr.org/wp-content/uploads/2023/11/jez-timms-vTKLujPJecw-unsplash-1536x1026.jpg");
+    background-repeat:no-repeat;
+    background-size:cover;
+  }
+
+  p{
+    color: white;
   }
 
   article {
@@ -58,25 +46,14 @@
     position: relative;
   }
 
-  .degree {
-    font-family: "Dancing Script", cursive;
-    font-size: 2em;
-  }
+  
 
   .right-side-text {
     margin-top: 10vh;
     margin-left: 5vw;
     text-align: left;
-    // margin-bottom: 0;
     font-size: 5em;
-    font-family: "Dancing Script", cursive;
+    font-family: 'Times New Roman', Times, serif;
   }
 
-  .img {
-    max-height: 45vh;
-    max-width: 35vw;
-    border-style: solid;
-    border-color: white;
-    border-width: 2px;
-  }
 </style>

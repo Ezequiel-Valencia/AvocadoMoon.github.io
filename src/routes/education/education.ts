06e574cbb502a5e2321cb3fd8e9b89f4fae14489
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-11-24 11:50:45 -0500

    Add Self Taught Section

---
+++ src/routes/education/education.ts
@@ -0,0 +1,27 @@
+
+
+export const technology_icons = [
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png",
+    "https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/css3-512.png",
+    "https://cdn-icons-png.flaticon.com/512/5968/5968381.png",
+    "https://raw.githubusercontent.com/sveltejs/svelte/29052aba7d0b78316d3a52aef1d7ddd54fe6ca84/site/static/images/svelte-android-chrome-512.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png",
+    "https://cdn-icons-png.flaticon.com/512/226/226777.png",
+    "https://tools.jboss.org/features/images/quarkus_icon_rgb_256px_default.png",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/512px-GraphQL_Logo.svg.png",
+    "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png",
+    "https://cdn2.iconfinder.com/data/icons/mixd/512/20_kubernetes-512.png",
+    "https://tobisyurt.net/assets/images/ansible-logo.png",
+    "https://seeklogo.com/images/I/istio-logo-92FF583709-seeklogo.com.png",
+    "https://upload.wikimedia.org/wikipedia/commons/3/3b/Grafana_icon.svg",
+    "https://grafana.com/media/docs/loki/logo-grafana-loki.png",
+    "https://global.discourse-cdn.com/flex015/uploads/imagej/original/3X/f/4/f41e19741763edf6f79e98aaf9259c614ddec98d.png",
+    "https://www.scottbrady91.com/img/logos/openid-icon.png",
+    "https://camo.githubusercontent.com/fdffb57ca7bf0ba2900bab738df7bf002dee35f15e55f2029a97de1d2bdc1e07/68747470733a2f2f7777772e70726f786d6f782e636f6d2f696d616765732f70726f786d6f782f50726f786d6f782d6c6f676f2d737461636b65642d38343070782e706e67",
+    "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/go-programming-language-icon.png",
+    "https://cdn.icon-icons.com/icons2/2415/PNG/512/django_line_logo_icon_146560.png"
+]
+
+
+
+

2efd4dbbf497022ebcd66d9594aa6c73fd078b89
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-11-28 08:29:14 -0500

    Basic Moon Reflection

--- src/routes/personal_projects/intro.svelte
+++ src/routes/personal_projects/intro.svelte
@@ -1,55 +1,104 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { onMount } from "svelte";
 
-<div id="intro-wrapper">
-  <div id="img-wrapper">
-    <img
-      id="intro-img"
-      src="/personal_projects/personal projects.png"
-      alt="Personal Projects"
-    />
+
+  function createReflections(reflections: NodeListOf<HTMLElement>, ocean_reflection: HTMLElement){
+    reflections.forEach((el) => {
+      let copy = el.cloneNode(false) as HTMLElement      
+      let yOffset =  Number(el.style.top.split("%")[0])
+      copy.style.top = ""
+      copy.style.bottom = yOffset + "%"
+      copy.style.position = "absolute"
+      copy.classList.add("reflected")
+      copy.id = ""
+      ocean_reflection.appendChild(copy)
+    })
+  }
+
+  onMount(() => {
+    let stars = document.querySelectorAll(".star") as NodeListOf<HTMLElement>
+    stars.forEach((star) => {
+      console.log("Star")
+      star.style.top = "" + Math.floor(Math.random() * 100) + "%"
+      star.style.left = "" + Math.floor(Math.random() * 100) + "%"
+     })
+
+    let ogMoon = document.querySelector("#og-moon") as HTMLElement;
+    ogMoon.style.top = 45 + "%";
+    const ocean_reflection = document.querySelector("#ocean") as HTMLElement
+    let reflections = document.querySelectorAll(".to-be-reflected") as NodeListOf<HTMLElement>
+    createReflections(reflections, ocean_reflection)
+    
+  })
+</script>
+
+<section id="intro-wrapper">
+  <div id="sky">
+    <img class="to-be-reflected" id="og-moon"
+        style="height: 10vmin; color:white; z-index:5; position:absolute;" 
+        src="./personal_projects/moon.svg" 
+        alt="Moon"
+    >
+    {#each {length: 30} as _}
+      <div class="to-be-reflected star"></div>
+    {/each}
   </div>
-  <div id="ethos">
-    <h3>
-      In my coding personal projects I try to experiement and let myself be creative. 
-      While before my projects where more short bursts of learning I now try to work on long
-      term projects.
-    </h3>
+  <div id="ocean">
+    
   </div>
-</div>
+  <div class="reflected"></div>
+</section>
 
 <style lang="scss">
-  #intro-img {
-    object-fit: inherit;
+  #intro-wrapper {
     height: 100vh;
     width: 100vw;
-    margin: 0;
-    padding: 0;
   }
 
-  #img-wrapper {
-    margin: 0;
-    background-color: #151f2b;
-    overflow: auto;
-  }
-
-  h3 {
-    color: white;
-    width: 80vw;
-    max-width: 1000px;
-    text-align: center;
-    margin: auto;
-  }
-
-  #ethos {
-    padding-top: 10%;
-    padding-bottom: 10%;
-    text-align: center;
-    background-color: #151f2b;
+  #sky{
     width: 100vw;
+    height: 50vh;
+    background-color: rgb(0, 0, 0);
+    background-image: radial-gradient(circle at 50% 50%, rgba(137, 43, 226, 0.397), rgba(6, 6, 109, 0.397), rgba(6, 6, 109, 0.185));
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: hidden;
   }
 
-  #intro-wrapper {
+  #ocean{
+    width: 100vw;
+    height: 50vh;
     position: relative;
-    height: auto;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+
+    background-color: rgb(0, 0, 0);
+    background-image: radial-gradient(circle at 50% 50%, 
+      rgba(180, 141, 216, 0.288), 
+      rgba(0, 0, 54, 0.63));
+    z-index: 1;
   }
+
+  .star{
+    width: 0.3em;
+    height: 0.3em;
+    background-color: white;
+    z-index: 1;
+    border-radius: 50%;
+    position: absolute;
+  }
+
+  .reflected{
+    transform: scale(1, -1);
+    // background-color: lightblue;
+    // border: 1px solid #000;
+    
+    opacity: 0.3;
+  }
+
+
 </style>

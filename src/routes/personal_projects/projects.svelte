c179841d6ff7e944ff6a471b13c4b3e825f558fd
Ezequiel Valencia <casualcucumber59@gmail.com>
2024-12-01 12:37:00 -0500

    Finalize Personal Projects Look

--- src/routes/personal_projects/projects.svelte
+++ src/routes/personal_projects/projects.svelte
@@ -3,12 +3,20 @@
     import Tabs from "../../common/tabs.svelte";
     import { projects } from "./project_object";
     import { importSCSSOptions } from "../../globals"
+  import { onMount } from "svelte";
     const sideImageLocation = "/personal_projects/animal star blue.png";
     const projectIterator = Object.entries(projects);
     let activeIndex = 0;
 
     const iconClass = "material-symbols-outlined"
     let icons = [[iconClass, "stat_3"], [iconClass, "stat_2"], [iconClass, "stat_1"]]
+
+    onMount(() => {
+        const clouds = document.querySelectorAll(".cloud") as NodeListOf<HTMLElement>
+        clouds.forEach((c) => {
+            c.style.setProperty("--time-dif", Math.floor((Math.random() * 1)) + "s")
+        })
+    })
 
 </script>
 
@@ -19,10 +27,20 @@
 
 <section>
     <section id="sunny-ocean-intro">
-        <div id="headline-text" style="text-align: center; padding-top:5%;">
-            <h2 style="">Personal Projects</h2>
-            <h3>Revealing an ocean of creativity from dreams</h3>
+        <div id="headline-text" style="text-align: center; padding-top:5%; z-index: 3; position:relative;">
+            <h1 style="font-size: xx-large;">Personal Projects</h1>
+            <u style="color: rgb(154, 204, 248);">
+                <h3>Revealing an ocean of creativity from dreams</h3>
+            </u>
+            
         </div>
+        <div id="sun"></div>
+        {#each {length: 7} as _, i}
+            <img class="cloud" src={"./personal_projects/cloud" + (i % 2) + ".svg"} 
+            alt={"cloud" + i} style="--left-p: {(Math.random() * 30) + (i * 10)}%; top: {Math.random() * 30}%"
+            >
+        {/each}
+
         <div id="ocean">
             <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
@@ -67,10 +85,60 @@
 
 
 <style lang="scss">
+    .cloud{
+        top: 15%; 
+        position:absolute; 
+        height:10vmin;
+        opacity: 15%;
+        left: var(--left-p);
+        animation: swayClouds  calc(20s + var(--time-dif)) linear alternate-reverse infinite;
+    }
+
+    @keyframes swayClouds{
+        from {
+            transform: translate(0, 0);
+        } to {
+            transform: translate(calc(100% + var(--left-p)), 0);
+        }
+    }
+
+    #sun{
+        left: 10%; 
+        top:10%; 
+        border-radius:50%; 
+        height: 10vmin; 
+        width:10vmin; 
+        background-color: rgba(255, 166, 0, 0.829); 
+        position:absolute;
+        z-index: 2;
+    }
+
+    #sun::after{
+        content: '';
+        position: absolute;
+        background-image: radial-gradient(orange, rgba(255, 166, 0, 0.767), transparent, transparent);
+        width: 170%;
+        height: 170%;
+        border-radius: 50%;
+        bottom: -35%;
+        left: -35%;
+    }
+
     #sunny-ocean-intro{
+        overflow: hidden;
         height: 100vh;
         width: 100vw;
-        background-image: linear-gradient(rgb(71, 113, 252), rgb(71, 113, 252), rgb(71, 113, 252), rgb(46, 68, 112));
+        background-image: linear-gradient(rgb(71, 113, 252), rgb(71, 113, 252), rgb(71, 113, 252), rgb(96, 144, 240));
+    }
+
+    #sunny-ocean-intro::after{
+        content: '';
+        position: absolute;
+        height: 70vh;
+        width: 100vw;
+        top: 0vh;
+        // z-index: 1;
+        // background-image: linear-gradient(transparent, rgba(255, 255, 255, 0.322));
     }
 
     #projects-wrapper{
@@ -124,7 +192,7 @@
         color: white;
     }
 
-    h2{
+    h2, h1{
         color: rgb(235, 245, 255);
         margin-top: 0;
     }
@@ -132,10 +200,10 @@
     .waves {
         position:absolute;
         width: 100%;
-        height: 20vh;
+        height: 30vh;
         margin-bottom:-7px; /*Fix for safari gap*/
         min-height:100px;
-        top: 80vh;
+        top: 70vh;
     }
 
     /* Animation */

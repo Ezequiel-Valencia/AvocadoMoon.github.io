7e0f4dcb4711c55d6f152223342f251bbd014bb2
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-01-15 19:26:25 -0500

    Quick Package

---
+++ src/routes/care_package/+page.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+</script>
+
+<section id="main-div">
+  <div id="text-div" style="">
+    <h1>Hidden amongst the starts:</h1>
+    <div style="width: 20vw; margin:auto;">
+      <ul style="text-align: center;">
+        <li>
+          <a href="https://youtu.be/ovW7yRi1iJ8">Cat music video</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/reel/DEuCV38uB7h/?igsh=a2w3M3M3ZWo0aW1h">Cat Edit</a>
+        </li>
+        <li>
+          <a href="https://kry.pt/">Terminal Site</a>
+        </li>
+        <li>
+          <a href="https://juneish.neocities.org/">Freak world</a>
+        </li>
+        <li>
+          <a href="https://youtu.be/ET2JmjbjE68">Ambient Music Mix :)</a>
+        </li>
+        <li>
+          <a href="https://youtu.be/Nrk8sqZfsgI">How to pronounce GIF</a>
+        </li>
+        <li>
+          <a href="https://scottts.itch.io/different-strokes">Interactive art gallery</a>
+        </li>
+        <li>
+          <a href="https://cosmicadventuresquad.itch.io/it-was-all-for-the-tuna">Fisherman</a>
+        </li>
+        <li>
+          <a href="https://anaissalla.itch.io/nomnom">Animal Chef Jam :0</a>
+        </li>
+        <li>
+          <a href="https://rewindgames.itch.io/tanuki-sunset"> Racoon tricks</a>
+        </li>
+        <li>
+          <a href="https://youtu.be/WSbUPaLTTUg">The sun is pretty.</a>
+        </li>
+        <li>
+          <a href="https://en.wikipedia.org/wiki/Kiki%27s_Delivery_Service">Flying animation</a>
+        </li>
+      </ul>
+    </div>
+  
+  </div>
+  
+</section>
+
+<style lang="scss">
+  #main-div {
+    background-image: url("/Backgrounds/stars.jpg");
+    height: auto;
+    min-height: 100vh;
+    width: 100vw;
+    align-items: center;
+    top: 0;
+    left: 0;
+    position: absolute;
+  }
+
+  #text-div{
+    text-align: center;
+    background-color: rgba(255, 255, 255, 0.911);
+    max-width: 80vw;
+    margin: auto;
+    border-radius: 5px;
+    position: relative;
+    // top: 5vh;
+    padding: 1%;
+  }
+
+  h1, li, a{
+    color: rgb(0, 0, 0);
+  }
+
+  li{
+    text-indent: -5%;
+    padding-top: 2vh;
+    padding-bottom: 2vh;
+    list-style-type: upper-roman;
+  }
+
+</style>

08fcbf0ff620bf387f69394d7dd02011ba164f77
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-02-09 16:52:02 -0500

    First Visit Sepearte File

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   import { musicController, sfxController, hasVisited } from "../common/myLocalStorage";
   import { onMount } from 'svelte';
   import { clickedOff } from '../globals'
+  import FirstVisit from './first-visit.svelte';
 
   onMount(async () => {
     document.addEventListener("click", (event) : void => {
@@ -34,36 +35,7 @@
   
 
   {#if !$hasVisited}
-    <div id="allowAudio" class="menu-popup">
-      <h2 class="menu-large-text">Allow Audio</h2>
-      <p class="menu-med-text" style="margin-left: 2%; margin-right: 2%;">
-        This website plays different background music and sound effects for each channel. 
-        Do you want to allow this audio?
-      </p>
-      {#if $musicController}
-        <u><p>Even if audio is turned on, autoplay needs to enabled in the browser.</p></u>
-      {/if}
-      <button
-        id="music"
-        class="music-option-buttons menu-med-text"
-        on:click={(e) => {
-          sfxController.toggle_sfx();
-          musicController.toggle_music();
-        }}
-      >
-        Turn Audio {$musicController ? "Off" : "On"}
-      </button>
-      
-      <h2 class="menu-large-text">Resume</h2>
-      <p style="margin-left: 2%; margin-right: 2%;" class="menu-med-text">For people who want to see everything I've done in the quickest manner possible.</p>
-      <a href="https://docs.google.com/document/d/e/2PACX-1vQXG6xgS-gXDlE0v03SPC5k56cUHKYMERYuDSOsqoyo8cLsFWslB_Rmr0B_Het3GDX4m7YfRlge-lbo/pub">
-      Link to Resume
-      </a>
-      <br>
-      <br>
-      <button class="music-option-buttons menu-med-text" on:click={(e) => {hasVisited.hasVisited()}}>Close</button>
-
-    </div>
+    <FirstVisit></FirstVisit>
   {/if}
 
   {#if $musicController}
@@ -79,7 +51,6 @@
 </div>
 
 <style lang="scss">
-  @use 'homeMenu';
   #mainDiv {
     height: 100vh;
     width: 100vw;

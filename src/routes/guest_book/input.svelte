a3300e3141016fb6d37049058812c46b3f49201b
Ezequiel Valencia <casualcucumber59@gmail.com>
2025-03-26 21:05:33 -0400

    Add Location Name

--- src/routes/guest_book/input.svelte
+++ src/routes/guest_book/input.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import { GeoCacheApi, type GeoCacheSubmission, type SendSubmissionRequest } from "../../backend-api";
 
-    let submissionReceived = false
     let geo = new GeoCacheApi()
     $: addSecret = false
+    $: addLocation = false
     $: grabbingLocation = false
 
     let latitude: number
@@ -13,9 +13,10 @@
         let username = document.querySelector<HTMLInputElement>("#username")!.value!
         let note = document.querySelector<HTMLTextAreaElement>("#note")!.value!
         let secretNode = document.querySelector<HTMLInputElement>("#secret")
+        let locationName = document.querySelector<HTMLInputElement>("#location-name")
         
         let submission: GeoCacheSubmission = {name: username, note: note, secret: secretNode != null ? secretNode.value : "", 
-        longitude: longitude + "", latitude: latitude + ""}
+        longitude: longitude, latitude: latitude, locationName: locationName != null ? locationName.value : ""}
         
         try{
             console.log(submission)
@@ -43,48 +44,55 @@
 </script>
 
 <section>
-    {#if submissionReceived}
-        <p>Submitted</p>
-    {:else}
-        <form on:submit={handleGuestSubmission}>
-            <label>
-                Formal Submission
-                <br>
-                <br>
-                <input required placeholder="Your Name" style="border-radius: 6px;" name="username" type="text" id="username">
-                <br>
-                <br>
+    <form onsubmit={handleGuestSubmission}>
+        <label>
+            Your Geocache Entry
+            <br>
+            <br>
+            <input required placeholder="Your Name" name="username" type="text" id="username">
+            <br>
+            <br>
+            <textarea required maxlength="250" placeholder="Your message (max 250 characters)." 
+            style="border-radius: 6px; width:30vw;" name="note" id="note"></textarea>
+            <br>
+            <br>
+            {#if addLocation}
                 {#if grabbingLocation}
                     <p>Grabbing Your Location</p>
                     <div class="loader"></div> 
                 {:else}
-                    <input defaultValue={latitude == undefined ? "" : latitude} placeholder="Latitude (optional)" style="border-radius: 6px;" name="latitude" type="text" id="latitude">
-                    <input defaultValue={longitude == undefined ? "" : longitude} placeholder="Longitude (optional)" style="border-radius: 6px;" name="longitude" type="text" id="longitude">
-                    <!-- svelte-ignore a11y_consider_explicit_label -->
-                    <button on:click={usersLocation} type="button" style='font-size:small'>
-                        <i class='fas fa-map-marker-alt'></i>
-                    </button>
+                    <div style="width: 30vw; text-align:center; margin:auto; display:inline;">
+                        <input maxlength="50" required style="width:30vw;" placeholder="Location Name" name="location-name" type="text" id="location-name">
+                        <br>
+                        <div style="width:30vw; display:inline-block;">
+                            <input required defaultValue={latitude == undefined ? "" : latitude} placeholder="Latitude" name="latitude" type="number" id="latitude">
+                            <input required defaultValue={longitude == undefined ? "" : longitude} placeholder="Longitude" name="longitude" type="number" id="longitude">
+                            <!-- svelte-ignore a11y_consider_explicit_label -->
+                            <button onclick={usersLocation} type="button" style='font-size:small'>
+                                <i class='fas fa-map-marker-alt'></i>
+                            </button>
+                        </div>
+                    </div>
                 {/if}
                 <br>
                 <br>
-                <textarea required maxlength="250" placeholder="Your message (max 250 characters)." 
-                style="border-radius: 6px; width:30vw;" name="note" id="note"></textarea>
+            {/if}
+            {#if addSecret}
+                <textarea maxlength="50" placeholder="Secret message (max 50 characters)." 
+                style="border-radius: 6px; width:30vw;" name="secret" id="secret"></textarea>
                 <br>
                 <br>
-                {#if addSecret}
-                    <textarea maxlength="50" placeholder="Secret message (max 50 characters)." 
-                    style="border-radius: 6px; width:30vw;" name="secret" id="secret"></textarea>
-                    <br>
-                    <br>
-                {/if}
-                <div style="width: 30vw; text-align:center; margin:auto;" class="flex items-center">
-                    <input on:change={() => {addSecret = !addSecret}} style="border: solid black 1px;" id="checked-checkbox" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                    <label for="checked-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Add Secret?</label>
-                </div>
-                <button>Submit Message</button>
-            </label>
-        </form>
-    {/if}
+            {/if}
+            <div style="width: 30vw; text-align:center; margin:auto;" class="flex items-center">
+                <button type="button" onclick={() => {addSecret = !addSecret}}>{addSecret ? "Remove" : "Add"} Secret?</button>
+                <button style="margin-left: 20px;" type="button" onclick={() => {addLocation = !addLocation}}>{addLocation ? "Remove" : "Add"} Location?</button>
+                <!-- <input on:change={() => {addSecret = !addSecret}} style="border: solid black 1px; border-radius: 1px;" id="checked-checkbox" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"> -->
+                <!-- <label for="checked-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Add Secret?</label> -->
+            </div>
+            <br>
+            <button>Submit Message</button>
+        </label>
+    </form>
 </section>
 
 
@@ -92,7 +100,11 @@
     form{
         text-align: center;
         margin: auto;
-        padding-top: 10vh;
+        padding-top: 5vh;
+    }
+
+    input {
+        border-radius: 6px;
     }
 
     button{

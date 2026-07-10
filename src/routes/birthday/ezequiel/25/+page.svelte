1cf265e75a23125d95e6abbe75d68732c387afb0
Ezequiel Valencia <ezq.valencia@gmail.com>
2026-07-09 20:27:16 -0400

    B

---
+++ src/routes/birthday/ezequiel/25/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<span id="main-div">
+  <div style="background-color: white;">
+  </div>
+
+</span>
+
+<style lang="scss">
+  #main-div {
+    // overflow:visible;
+    position: absolute;
+    left: 0%;
+    top: 0%;
+    height: 100%;
+    width: 100%;
+    background-image: url('/birthday/Birthday Invite.png');
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+  }
+</style>
